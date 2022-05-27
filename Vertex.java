984ee915eaf225719d3933aba273ae33abc77440
nukagit <104043152+nukagit@users.noreply.github.com>
2022-05-27 23:24:48 +0600

    Add files via upload

---
+++ Vertex.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Vertex<T> {
+    private T data;
+    private Map<Vertex<T>, Double> adjacentVertices;
+
+    public Vertex (T data){
+        this.data=data;
+        adjacentVertices= new HashMap<>();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex<?> vertex = (Vertex<?>) o;
+        return Objects.equals(data, vertex.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(data, adjacentVertices);
+    }
+}
